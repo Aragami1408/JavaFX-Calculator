7abfe407f6713dfd3bd1942349579d46312a5745
Aragami1408 <vucaominh1408@gmail.com>
2018-02-15 18:01:41 +0700

    Replace author

--- src/Controller.java
+++ src/Controller.java
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 /**
- * @author Villan
+ * @author Aragami1408
  */
 public class Controller implements Initializable {
 
